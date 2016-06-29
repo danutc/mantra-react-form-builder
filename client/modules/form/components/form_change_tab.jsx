d98a24f111cf0d0ec514feefdb9f775726c22d93
Thai Tran <thaitcao@gmail.com>
2016-06-29 16:01:33 +1000

    - change the default string for onchange

--- client/modules/form/components/form_change_tab.jsx
+++ client/modules/form/components/form_change_tab.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormTextEditor from '../containers/form_text_editor.js';
 
-var onChangeSnippets = 'function onChange(event) {\n\tconsole.log(event.target.value);\n}\n';
+var onChangeSnippets = 'function onChange(data) {\n\tconsole.log(data);\n}\n';
 
 class FormChangeTab extends React.Component {
   constructor(props) {
