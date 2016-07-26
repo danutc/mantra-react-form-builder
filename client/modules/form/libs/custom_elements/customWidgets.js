9b5447b2ca1ec9a6ffcaa42784694af18f56fb04
Thai Tran <thaitcao@gmail.com>
2016-07-26 15:14:52 +1000

    - return value from the wysiwyg

--- client/modules/form/libs/custom_elements/customWidgets.js
+++ client/modules/form/libs/custom_elements/customWidgets.js
@@ -1,40 +1,39 @@
-import assert from 'assert';
-import React from 'react';
-import Wysiwyg from './wysiwyg';
+import assert from 'assert'
+import React from 'react'
+import Wysiwyg from './wysiwyg'
 
 let WysiwygWidget = (props) => {
-    const {description, value, defaultValue, required, onChange} = props
+  const {description, value, defaultValue, required, onChange} = props
 
-    return React.createElement(Wysiwyg, {
-        onChange: function onChange (event) {
-            onChange(event.target.value)
-        },
-        value: value,
-        required: required,
-        placeholder: description,
-        defaultValue: defaultValue
-    })
-};
+  return React.createElement(Wysiwyg, {
+    onChange: function onChange (editorState) {
+      console.log(editorState.getCurrentContent().getPlainText())
+    },
+    value: 'test',
+    required: required,
+    placeholder: description,
+    defaultValue: defaultValue
+  })
+}
 
 const widgetsMap = {
-    'wysiwyg': WysiwygWidget
-};
+  'wysiwyg': WysiwygWidget
+}
 
-export default function deepSchemaLookup(inputSchema) {
+export default function deepSchemaLookup (inputSchema) {
+  assert(typeof inputSchema === 'object', 'inputSchema should be an object')
 
-    assert(typeof inputSchema === 'object', 'inputSchema should be an object');
+  Object.keys(inputSchema).forEach((value) => {
+    let node = inputSchema[value]
+    if (typeof node === 'object') {
+      inputSchema[value] = deepSchemaLookup(node)
+    } else if (typeof node === 'string'
+      && node.indexOf('widgetLoader::') !== -1
+      && widgetsMap.hasOwnProperty(node.replace('widgetLoader::', ''))
+    ) {
+      inputSchema[value] = widgetsMap[node.replace('widgetLoader::', '')]
+    }
+  })
 
-    Object.keys(inputSchema).forEach((value)=> {
-        let node = inputSchema[value];
-        if (typeof node === 'object') {
-            inputSchema[value] = deepSchemaLookup(node);
-        } else if (typeof node === 'string'
-            && node.indexOf('widgetLoader::') !== -1
-            && widgetsMap.hasOwnProperty(node.replace('widgetLoader::', ''))
-        ) {
-            inputSchema[value] = widgetsMap[node.replace('widgetLoader::', '')];
-        }
-    });
-
-    return inputSchema;
+  return inputSchema
 }
