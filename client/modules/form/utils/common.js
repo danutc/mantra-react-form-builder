bb2411db7202be02c4385eae2d54e3d8c8fcdbfb
Thai Tran <thaitcao@gmail.com>
2016-08-08 13:58:36 +1000

    -

--- client/modules/form/utils/common.js
+++ client/modules/form/utils/common.js
@@ -40,14 +40,14 @@ let convertElement = (e, parents, key) => {
   let obj = null
   if (e && e['ui'] && e['ui']['ui:widget'] == 'array') {
     let depth = e['def']['ext'] ? e['def']['ext']['depth'] : 0;
-    obj = {key: key, type: 'array', depth: depth}
+    obj = { key: key, type: 'array' }
     e['def'] = convertEleToArr(e['def'])
   } else if (e && e['ui'] && e['ui']['ui:widget'] == 'object') {
     let depth = e['def']['ext'] ? e['def']['ext']['depth'] : 0;
-    obj = {key: key, type: 'object', depth: depth} 
+    obj = { key: key, type: 'object' }
     e['def'] = convertEleToObj(e['def'])
-  } 
-  
+  }
+
   if (obj) parents.push(obj)
 }
 
@@ -55,37 +55,50 @@ let buildTree = (key, ele, parents, scheme, ui) => {
   // if no depth, then push to 
   if (ele['def']['ext'] == null || ele['def']['ext']['depth'] == 0) {
     scheme[key] = ele['def'];
-    ui[key] = ele['ui'];
+
+    if (ele['def']['type'] == 'array') {
+      ui[key] = { items: {} }
+    } else if (ele['def']['type'] == 'object') {
+      ui[key] = {}
+    } else {
+      ui[key] = ele['ui'];
+    }
+    
   } else {
-  
     // now jump from the top of the tree to the correct node. at this moment, the parent already have 
     // the information of the parent node 
-    let s = scheme; 
+    let s = scheme;
+    let u = ui;
 
     for (let node of parents) {
       // we need to prevent the current node is also the array, 
       // because it is already pushed to the stack but does not  actually
       // populate to the tree, so we dont jump 
-      if (node['key'] == key) break; 
+      if (node['key'] == key) break;
 
       // jump to the deadend in order to prepare for the next element
       if (node['type'] == 'object') {
         s = s[node['key']]['properties']
+        u = u[node['key']]
       } else if (node['type'] == 'array') {
         s = s[node['key']]['items']['properties']
+        u = u[node['key']]['items']
       }
     }
-    
+
     // if the element is the container, then populate the container 
     // schema into the tree, otherwise, populate the element def 
     // into the tree 
     if (ele['def']['type'] == 'array') {
-      s[key] = arraySchema; 
+      s[key] = arraySchema;
+      u[key] = { 'items': {} };
     } else if (ele['def']['type'] == 'object') {
       s[key] = objSchema;
+      u[key] = {};
     } else {
       // now safely and nicely insert the element
       s[key] = ele['def']
+      u[key] = ele['ui']
     }
   }
 }
@@ -102,28 +115,26 @@ let buildForm = (form_fields) => {
     let element = form_fields[key];
 
     let depth = element['def']['ext'] ? element['def']['ext']['depth'] : 0;
-    
-    // check if there is the change in the depth, if the depth is decrease, then pointer
-    // moved to the previous level, thus remove the last parent 
-    let parent_no = parents.length;  
-    let distance = parent_no - depth; 
+    let parent_no = parents.length;
+    //  
+    let distance = parent_no - depth;
 
-    for (let i = 0; i < distance; i ++) {
+    for (let i = 0; i < distance; i++) {
       parents.pop();
     }
 
     convertElement(element, parents, key);
-    buildTree(key, element, parents, schema, ui);    
+    buildTree(key, element, parents, schema, ui);
   }
 
   console.log('... final schema ... ')
   console.log(schema)
 
-  // console.log('... final ui ...')
-  // console.log(ui)
+  console.log('... final ui ...')
+  console.log(ui)
 
   finalForm['schema'] = { type: 'object', properties: { ...schema } }
-  finalForm['ui'] = {}
+  finalForm['ui'] = ui
   finalForm['widgets'] = {}
 
   return finalForm
@@ -139,4 +150,4 @@ let computeFinalForm = (LocalState) => {
   return finalForm
 }
 
-export default {computeFinalForm}
+export default { computeFinalForm }
